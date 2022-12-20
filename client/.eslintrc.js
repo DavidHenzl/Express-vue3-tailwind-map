568ba67c8cb0c4a7332f8ed65a37a2a5b39ea8f9
David Henzl <david.henzl@plus4u.net>
2022-12-20 10:05:44 +0100

    first commit

---
+++ client/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    'vue/setup-compiler-macros': true // defineProps no longer needs import and this fixes the error
+  },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
+    
+  }
+}
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="h-screen relative">
+    <GeoErrorModal
+      @closeGeoError="closeGeoError"
+      v-if="geoError"
+      :geoErrorMsg="geoErrorMsg"
+    />
+    <MapFeatures
+      @getGeolocation="getGeolocation"
+      @plotResult="plotResult"
+      @toggleSearchResults="toggleSearchResults"
+      @removeResult="removeResult"
+      :coords="coords"
+      :fetchCoords="fetchCoords"
+      :searchResults="searchResults"
+    />
+    <div id="map" class="h-full z-[1]"></div>
+  </div>
+</template>
+
+<script setup>
+import leaflet from 'leaflet';
+import { onMounted, ref } from 'vue';
+import GeoErrorModal from '@/components/GeoErrorModal.vue'
+import MapFeatures from '@/components/MapFeatures.vue'
+
+let map;
+
+onMounted(() => {
+  // init map
+  map = leaflet.map('map').setView([51.505, -0.09], 10);
+
+  // add tile layer
+  leaflet
+    .tileLayer(
+      'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }
+    )
+    .addTo(map);
+
+  map.on('moveend', () => {
+    closeSearchResults();
+  })
+
+  getGeolocation();
+})
+
+const coords = ref(null);
+const fetchCoords = ref(null);
+const geoMarker = ref(null);
+const geoError = ref(null);
+const geoErrorMsg = ref("this is error msg");
+
+const getGeolocation = () => {
+  if (coords.value) {
+    coords.value = null;
+    sessionStorage.removeItem('coords');
+    map.removeLayer(geoMarker.value);
+    return
+  }
+  // check session storage for coords
+  if (sessionStorage.getItem('coords')) {
+    coords.value = JSON.parse(sessionStorage.getItem('coords'));
+    plotGeolocation(coords.value);
+    return
+  }
+
+  fetchCoords.value = true;
+  navigator.geolocation.getCurrentPosition(setCoords, getLocError);
+}
+
+const setCoords = (pos) => {
+  // stop fetching coords
+  fetchCoords.value = null;
+
+  // set coords in session storage
+  const setSessionCoords = {
+    lat: pos.coords.latitude,
+    lng: pos.coords.longitude,
+  }
+
+  sessionStorage.setItem('coords', JSON.stringify(setSessionCoords));
+
+  // set ref coords value
+  coords.value = setSessionCoords;
+
+  plotGeolocation(coords.value);
+}
+
+const getLocError = (err) => {
+  fetchCoords.value = null;
+  geoError.value = true;
+  geoErrorMsg.value = err.message;
+}
+
+const plotGeolocation = (coords) => {
+  // create custom marker
+  const customMarker = leaflet.icon({
+    iconUrl: require('../assets/map-marker-red.svg'),
+    iconSize: [35, 35]
+  })
+
+  // create new marker with coords and custom marker
+  geoMarker.value = leaflet
+    .marker([coords.lat, coords.lng], { icon: customMarker })
+    .addTo(map);
+
+  // set map view to current location
+  map.setView([coords.lat, coords.lng], 10);
+
+
+}
+
+const closeGeoError = () => {
+  geoError.value = null;
+  geoErrorMsg.value = null;
+}
+
+const resultMarker = ref(null);
+const plotResult = (coords) => {
+  // Check to see if resultMarker has value
+  if (resultMarker.value) {
+    map.removeLayer(resultMarker.value);
+  }
+
+  // create custom marker
+  const customMarker = leaflet.icon({
+    iconUrl: require('../assets/map-marker-blue.svg'),
+    iconSize: [35, 35]
+  })
+
+  // create new marker with coords and custom marker
+  resultMarker.value = leaflet
+    .marker([coords.coordinates[1], coords.coordinates[0]], { icon: customMarker })
+    .addTo(map);
+
+  // set map view to current location
+  map.setView([coords.coordinates[1], coords.coordinates[0]], 14);
+
+  closeSearchResults();
+}
+
+const searchResults = ref(null);
+const toggleSearchResults = () => {
+  searchResults.value = !searchResults.value;
+}
+
+const closeSearchResults = () => {
+  searchResults.value = null;
+}
+
+const removeResult = () => {
+  map.removeLayer(resultMarker.value);
+}
+
+</script>
